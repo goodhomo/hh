7da07dc82a34df39cee02d6f1f33d3c5b76db2bf
goodhomo <116372489+goodhomo@users.noreply.github.com>
2024-05-25 11:00:46 +0800

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,18 +1,21 @@
 <?php
-{
-$file = $_FILES['hidden'];
+error_reporting(0);
+$base64_img = trim($_POST['img']);
+$id = trim($_GET['id']);
+$up_dir = './img/';//存放在当前目录的img文件夹下
 
-$fileName = $_FILES['hidden']['img']
-$fileTmpName = $_FILES['hidden']['tmp_name']
-$fileSize = $_FILES['hidden']['size']
-$fileType = $_FILES['hidden']['type']
 
-$fileExt = explode('.',$fileName);
-$fileActualExt = strtolower(end($fileExt));
-$allowed = array('jpg','png','jpeg');
-if ($fileSize < 50000)
-$fileNameNew = uniqid('',true).".".$fileActualExt;
-$fileDestination = 'files/'.$fileNameNew;
-move_uploaded_file($fileTmpName,$fileDestination);
-  header("Location:index.php?ooo");
+
+if(!file_exists($up_dir)){
+  mkdir($up_dir,0777);
 }
+
+if(preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_img, $result)){
+  $type = $result[2];
+  if(in_array($type,array('bmp','png'))){
+    $new_file = $up_dir.$id.'_'.date('mdHis_').'.'.$type;
+    file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_img)));
+    header("Location:index.html?url=https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4?authorization=bce-auth-v1%2F40f207e648424f47b2e3dfbb1014b1a5%2F2021-07-12T02%3A14%3A24Z%2F-1%2Fhost%2F530146520a1c89fb727fbbdb8a0e0c98ec69955459aed4b1c8e00839187536c9");
+  }
+}
+?>
